4676835a24a2c73ece709c8da239f562a81c7249
Heisey <jheisler01@gmail.com>
2024-07-19 06:05:16 -0700

    touchup

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -22,7 +22,7 @@ const About: React.FC<AboutProps> = (props) => {
         <p className='message space'>This app was created as a interview assignment for Speer Technologies. I really enjoyed working on the assignment, and I hope you enjoy my work.</p>
 
         <p className='message'>The Original Assignment object have been meet:</p>
-        <ul className='list spacer'>
+        <ul className='list space'>
           <li className='list__item'><Icons.Solid.Check fill={theme.current.border.color} className='list__icon' size='25px' /><span className='list__text'>Call Feed Created with all messages that have not been archived</span></li>
           <li className='list__item'><Icons.Solid.Check fill={theme.current.border.color} className='list__icon' size='25px' /><span className='list__text'>Call Feed has the ability to archive all calls, also some filtering has been added</span></li>
           <li className='list__item'><Icons.Solid.Check fill={theme.current.border.color} className='list__icon' size='25px' /><span className='list__text'>Call Details where you have the ablility to archive/unarchive a single call</span></li>
@@ -32,27 +32,30 @@ const About: React.FC<AboutProps> = (props) => {
         </ul>
 
         <p className='message'>Additions i Made that was not part of the assignment, I had extra time so I thought I would add some extra features:</p>
-        <ul className='list spacer'>
+        <ul className='list space'>
           <li className='list__item'><Icons.Solid.Star fill='#edd713' className='list__icon' size='25px' /><span className='list__text'>Light and Dark themes have been added, with a setting to toggle between</span></li>
           <li className='list__item'><Icons.Solid.Star fill='#edd713' className='list__icon' size='25px' /><span className='list__text'>Cached storage using local storage to save settings in case of refresh</span></li>
           <li className='list__item'><Icons.Solid.Star fill='#edd713' className='list__icon' size='25px' /><span className='list__text'>A Guided Tour of the app</span></li>
         </ul>
 
-        {/* <p className='message'>What I ran out of time for:</p>
-        <ul className='list spacer'>
-          <li className='list__item'><Icons.Solid.Sad fill={theme.current.button.variants.danger.fg} className='list__icon' size='25px' /><span className='list__text'></span></li>
-          <li className='list__item'><Icons.Solid.Sad fill={theme.current.button.variants.danger.fg} className='list__icon' size='25px' /><span className='list__text'>Cached storage using local storage to save settings in case of refresh</span></li>
-          <li className='list__item'><Icons.Solid.Sad fill={theme.current.button.variants.danger.fg} className='list__icon' size='25px' /><span className='list__text'>A Guided Tour of the app</span></li>
-        </ul> */}
+        <p className='message'>What I ran out of time for:</p>
+        <ul className='list space'>
+          <li className='list__item'><Icons.Solid.Sad fill={theme.current.button.variants.danger.fg} className='list__icon' size='25px' /><span className='list__text'>Adding Tutorial to Mobile Size, and adjusting some setting for Tutorial for the rest of the app</span></li>
+          <li className='list__item'><Icons.Solid.Sad fill={theme.current.button.variants.danger.fg} className='list__icon' size='25px' /><span className='list__text'>Localization, almost got there, then decided I wanted to go to sleep</span></li>
+        </ul>
 
         <p className='message'>Here are also some of my links for you, you can find all my latest work here</p>
-        <div className='links'>
+        <div className='links space'>
           <a href='https://www.justinheisler.me/'><Icons.Solid.Globe fill={theme.current.border.color} size='45px' /></a>
           <a href='https://github.com/Heisey'><Icons.Solid.GitHub fill='#FF5630' size='45px' /></a>
           <a href='https://www.linkedin.com/in/justin-heisler-35069a87/'><Icons.Solid.Linkedin fill='#0077B5' size='45px' /></a>
         </div>
+
+        <p className='message'>Hopefully you enjoyed everything and I hope to hear from you soon</p>
+        <p className='message'> Submitted on July 19, 2024 at 7AM PST</p>
+        <p className='message'>Cheers</p>
+        <p className='message'>Justin Heisler</p>
       </div>
-      
     </Styles.About>
   )
 }
